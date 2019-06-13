4af776c486f4b911657be5a4ebf5c1771b19371f
Josh Downey <downey.josh@gmail.com>
2019-06-13 12:58:27 -0400

    Capture network requests
    Add listener for onRequestFinished and set request details in state.
    Render basic request info in a table.
    Set up some basic table styling.
    Ignore annoying eslint rules.

--- src/components/GraphQLTab.jsx
+++ src/components/GraphQLTab.jsx
@@ -5,12 +5,37 @@ import styles from "./GraphQLTab.css";
 
 export default class GraphQLTab extends React.Component {
   state = {
-    greeting: "Whassup Fresh? It's our turn, baby"
+    requests: []
   };
+
+  componentDidMount() {
+    const { onRequestFinished } = this.props;
+    onRequestFinished.addListener(request => this.handleRequest(request));
+  }
+
+  handleRequest(request) {
+    console.log(request);
+    request.getContent(content => {
+      this.setState(prev => {
+        const prevRequests = prev.requests;
+        const nextRequest = {
+          request: request.request,
+          response: request.response,
+          time: Math.round(request.time),
+          startedDateTime: request.startedDateTime,
+          content: JSON.parse(content)
+        };
+        return {
+          ...prev,
+          requests: [...prevRequests, nextRequest]
+        };
+      });
+    });
+  }
 
   render() {
     const { theme } = this.props;
-    const { greeting } = this.state;
+    const { requests } = this.state;
 
     const themeClass = clsx({
       [styles.dark]: theme === "dark"
@@ -18,12 +43,36 @@ export default class GraphQLTab extends React.Component {
 
     return (
       <div className={themeClass}>
-        <h1 className={styles.greets}>{`${greeting}`}</h1>
+        <table>
+          <thead>
+            <th>Operation</th>
+            <th>Warnings</th>
+            <th>Errors</th>
+            <th>Status</th>
+            <th>Time</th>
+            <th>Method</th>
+            <th>Type</th>
+          </thead>
+          <tbody>
+            {requests.map(({ request, response, time }) => (
+              <tr>
+                <td>Operation name goes here</td>
+                <td>0</td>
+                <td>0</td>
+                <td>{response.status}</td>
+                <td>{`${time} ms`}</td>
+                <td>{request.method}</td>
+                <td>{response.content.mimeType}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
 GraphQLTab.propTypes = {
+  onRequestFinished: PropTypes.func.isRequired,
   theme: PropTypes.string.isRequired
 };
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom";
 import GraphQLTab from "./components/GraphQLTab";
 import "./assets/css/main.css";
 
+
 function initializePanel() {
   // eslint-disable-next-line no-undef
-  const { create, themeName } = chrome.devtools.panels;
+  const { panels, network } = chrome.devtools;
+  const { create, themeName } = panels;
+  const { onRequestFinished } = network;
 
   create("GraphQL", null, "./dist/index.html", ({ onShown }) => {
     onShown.addListener(({ document }) => {
-      ReactDOM.render(<GraphQLTab theme={themeName} />, document.getElementById("root"));
+      ReactDOM.render(
+        <GraphQLTab
+          theme={themeName}
+          onRequestFinished={onRequestFinished}
+        />,
+        document.getElementById("root")
+      );
     });
   });
 }
