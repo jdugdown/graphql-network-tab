f64240183f9dda223ea0f243ed5bc143f80dfe05
Joshua Downey (IT - NGSS) <downey.josh@gmail.com>
2019-06-23 14:16:10 -0400

    Conditionally display variables in response tab and swap order of request and response tab content to match order of tabbed nav

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
+import isNil from "lodash/isNil";
+import isEmpty from "lodash/isEmpty";
 import { print } from "graphql/language/printer";
 import gql from "graphql-tag";
 import HeadersTable from "../HeadersTable/HeadersTable";
@@ -43,17 +45,7 @@ export default function SidePanel({ request, response, content, query, clearSele
       </div>
 
       <div className={styles.tabContent}>
-        {activeTab === REQUEST ? (
-          <>
-            <h3>Query</h3>
-            <pre className={styles.codeBlock}>
-              <code>{formattedQuery}</code>
-            </pre>
-
-            <h3>Headers</h3>
-            <HeadersTable headers={request.headers} />
-          </>
-        ) : (
+        {activeTab === RESPONSE ? (
           <>
             <h3>Response Data</h3>
             <pre className={styles.codeBlock}>
@@ -62,6 +54,25 @@ export default function SidePanel({ request, response, content, query, clearSele
 
             <h3>Headers</h3>
             <HeadersTable headers={response.headers} />
+          </>
+        ) : (
+          <>
+            <h3>Query</h3>
+            <pre className={styles.codeBlock}>
+              <code>{formattedQuery}</code>
+            </pre>
+
+            {!isNil(query.variables) && !isEmpty(query.variables) && (
+              <>
+                <h3>Variables</h3>
+                <pre className={styles.codeBlock}>
+                  <code>{JSON.stringify(query.variables, null, 2)}</code>
+                </pre>
+              </>
+            )}
+
+            <h3>Headers</h3>
+            <HeadersTable headers={request.headers} />
           </>
         )}
       </div>
@@ -90,7 +101,8 @@ SidePanel.propTypes = {
     data: PropTypes.shape({})
   }).isRequired,
   query: PropTypes.shape({
-    query: PropTypes.string.isRequired
+    query: PropTypes.string.isRequired,
+    variables: PropTypes.shape({})
   }).isRequired,
   clearSelectedRequest: PropTypes.func.isRequired
 };
